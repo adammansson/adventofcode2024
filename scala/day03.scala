aab1bb1f02693153279385b8987dd645c9b3f4e5
Adam Månsson <adam.s.mansson@gmail.com>
2024-12-03 21:48:20 +0100

    Refactor day03 in Scala again

--- scala/day03.scala
+++ scala/day03.scala
@@ -5,27 +5,23 @@ import scala.util.matching.Regex
 object Day03:
 	val path = os.pwd / ".." / "input" / "day03" / "input1.txt"
 	val lines = os.read.lines(path).toVector
+	val line = lines.mkString
 
 	def partOne(): Unit =
-		val line = lines.mkString
-
 		val result =
 			"mul\\(([0-9]+),([0-9]+)\\)".r
 			.findAllMatchIn(line)
-			.map(m => m.group(1).toInt * m.group(2).toInt)
-			.sum
+			.foldLeft(0)((acc, m) => acc + m.group(1).toInt * m.group(2).toInt)
 
 		println(result)
 
 	def partTwo(): Unit =
-		var line = lines.mkString
-
 		val result =
 			"mul\\(([0-9]+),([0-9]+)\\)|do\\(\\)|don't\\(\\)".r
 			.findAllMatchIn(line)
 			.foldLeft((true, 0))((t, s) => s match
-				case _ if s.toString == "do()" => (true, t._2)
-				case _ if s.toString == "don't()" => (false, t._2)
+				case _ if s.matched == "do()" => (true, t._2)
+				case _ if s.matched == "don't()" => (false, t._2)
 				case m =>
 					if t._1 then
 						(t._1, t._2 + m.group(1).toInt * m.group(2).toInt)

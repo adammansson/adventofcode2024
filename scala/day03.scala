b2308b44db7dd253232685704f4e5f5712eca5b4
Adam Månsson <adam.s.mansson@gmail.com>
2024-12-03 21:40:16 +0100

    Refactor day03 in Scala

--- scala/day03.scala
+++ scala/day03.scala
@@ -6,37 +6,34 @@ object Day03:
 	val path = os.pwd / ".." / "input" / "day03" / "input1.txt"
 	val lines = os.read.lines(path).toVector
 
-	val numberPattern: Regex = "mul\\([0-9]+,[0-9]+\\)".r
-
-	def performMul(s: String): Int =
-		s.drop(4).dropRight(1).split(",").map(_.toInt).reduce((acc, i) => acc * i)
-
 	def partOne(): Unit =
 		val line = lines.mkString
-		val result = numberPattern.findAllIn(line).map(performMul).sum
+
+		val result =
+			"mul\\(([0-9]+),([0-9]+)\\)".r
+			.findAllMatchIn(line)
+			.map(m => m.group(1).toInt * m.group(2).toInt)
+			.sum
+
 		println(result)
-
-	def solveTwo(line: String, enabled: Boolean, acc: Int): Int =
-		numberPattern.findFirstIn(line) match
-			case Some(mulmatch) => 
-				if line.startsWith(mulmatch) then
-					if enabled then
-						solveTwo(line.drop(mulmatch.length), enabled, acc + performMul(mulmatch))
-					else
-						solveTwo(line.drop(mulmatch.length), enabled, acc)
-				else if line.startsWith("do()") then
-					solveTwo(line.drop("do()".length), true, acc)
-				else if line.startsWith("don't()") then
-					solveTwo(line.drop("dont'()".length), false, acc)
-				else
-					solveTwo(line.tail, enabled, acc)
-
-			case None => acc
 
 	def partTwo(): Unit =
 		var line = lines.mkString
-		val result = solveTwo(line, true, 0)
-		println(result)
+
+		val result =
+			"mul\\(([0-9]+),([0-9]+)\\)|do\\(\\)|don't\\(\\)".r
+			.findAllMatchIn(line)
+			.foldLeft((true, 0))((t, s) => s match
+				case _ if s.toString == "do()" => (true, t._2)
+				case _ if s.toString == "don't()" => (false, t._2)
+				case m =>
+					if t._1 then
+						(t._1, t._2 + m.group(1).toInt * m.group(2).toInt)
+					else
+						(t._1, t._2)
+			)
+
+		println(result._2)
 
 	@main def run(): Unit =
 		partOne()

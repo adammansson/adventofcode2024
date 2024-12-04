bb6a6dddfe79ec0bd6083aae56dc24166adc1f41
Adam Månsson <adam.s.mansson@gmail.com>
2024-12-04 13:41:14 +0100

    Add alternative to day04p1 in Scala

--- scala/day04.scala
+++ scala/day04.scala
@@ -2,15 +2,17 @@
 
 object Day04:
     val path = os.pwd / ".." / "input" / "day04" / "input1.txt"
-    var input = os.read.lines(path).map(_.toVector).toVector
+    val input = os.read.lines(path).map(_.toVector).toVector
+    var matrix = input
 
     val paddingAmount = 3
-    val paddingRow = Vector.fill(input(0).length)('#')
+    val paddingRow = Vector.fill(matrix(0).length)('#')
     val paddingRows = Vector.fill(paddingAmount)(paddingRow)
 
-    input = paddingRows ++ input ++ paddingRows
-    input = input.map(row => "###".toVector ++ row ++ "###".toVector)
-    val matrix = input
+    matrix = paddingRows ++ matrix ++ paddingRows
+    matrix = matrix.map(row =>
+        Vector.fill(paddingAmount)('#') ++ row ++ Vector.fill(paddingAmount)('#')
+    )
 
     def partOne(): Unit =
         var counter = 0
@@ -43,7 +45,27 @@ object Day04:
                         counter += 1
 
         println(counter)
+    
+
+    def partOneAlternative(): Unit =
+        def countXMAS(xs: Vector[Char]): Int =
+            xs.mkString.sliding(4).count(window => window == "XMAS" || window == "SAMX")
+
+        def countHorizontal(xs: Vector[Vector[Char]]): Int =
+            xs.map(countXMAS).sum
+
+        def countDiagonal(xs: Vector[Vector[Char]], padFn: (Vector[Char], Int) => Vector[Char]): Int =
+            input.sliding(4).map(_.zipWithIndex.map((x, i) => padFn(x, i))).map(xs => countHorizontal(xs.transpose)).sum
+
+        val result = 
+            countHorizontal(input) +
+            countHorizontal(input.transpose) +
+            countDiagonal(input, (x, i) => x.drop(i) ++ Vector.fill(i)('#')) +
+            countDiagonal(input, (x, i) => Vector.fill(i)('#') ++ x.dropRight(i))
+
+        println(result)
 
     @main def run(): Unit =
         partOne()
         partTwo()
+        partOneAlternative()

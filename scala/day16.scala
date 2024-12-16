40c2bb95eda3f3a83d82b501df85c8f2cf05b01e
Adam Månsson <adam.s.mansson@gmail.com>
2024-12-16 21:11:09 +0100

    Solve day16p2 in Scala

--- scala/day16.scala
+++ scala/day16.scala
@@ -23,28 +23,50 @@ object Day16:
             data.map(_.mkString).mkString("\n")
 
     val path = os.pwd / ".." / "input" / "day16" / "input1.txt"
-    val input: Array[Array[Char]] = os.read.lines(path).map(_.toCharArray).toArray
+    val input: Vector[Vector[Char]] = os.read.lines(path).map(_.toVector).toVector
+    val dirs = Seq(Complex(1, 0), Complex(-1, 0), Complex(0, 1), Complex(0, -1))
+    val map = Matrix(input)
+    val start: Complex = map.find('S')
+    val end: Complex = map.find('E')
 
-    def partOne(): Long =
-        val map = Matrix(input.map(_.toVector).toVector)
-        val start: Complex = map.find('S')
-        val end: Complex = map.find('E')
-
-        val lowest = mutable.Map[(Complex, Complex), Long]()
+    val seen: Map[(Complex, Complex), Long] =
+        val currentlySeen = mutable.Map[(Complex, Complex), Long]()
         val queue = mutable.Queue[(Complex, Complex, Long)]((start, Complex(1, 0), 0))
-
         while queue.nonEmpty do
             val (pos, dir, points) = queue.dequeue
-            if lowest.getOrElse((pos, dir), Long.MaxValue) > points then
-                lowest((pos, dir)) = points
+            if currentlySeen.getOrElse((pos, dir), Long.MaxValue) > points then
+                currentlySeen((pos, dir)) = points
                 if map(pos + dir) != '#' then
                     queue.enqueue((pos + dir, dir, points + 1))
                 queue.enqueue((pos, dir * Complex(0, 1), points + 1000))
                 queue.enqueue((pos, dir * Complex(0, -1), points + 1000))
 
+        currentlySeen.toMap
 
-        lowest.foldLeft(Long.MaxValue)((acc, kv) =>
-            if kv._1._1 == end && acc > kv._2 then kv._2 else acc)
+    def bestdir(pos: Complex): Complex =
+        val min = dirs.map(dir => seen(pos, dir)).min
+        dirs.find(dir => seen(pos, dir) == min).get
+
+    def partOne(): Long =
+        seen((end, bestdir(end)))
+
+    def partTwo(): Long =
+        val bestpath = mutable.Set[Complex]()
+        val backtrack = mutable.Queue[(Complex, Complex)]((end, bestdir(end)))
+        while backtrack.nonEmpty do
+            val (pos, prevdir) = backtrack.dequeue
+            bestpath.add(pos)
+            if pos != start then
+                val dir = bestdir(pos) * Complex(-1, 0)
+                backtrack.enqueue((pos + dir, dir))
+
+                if dir != prevdir &&
+                   map(pos + prevdir) != '#' &&
+                   seen(pos + dir, dir * Complex(-1, 0)) + 1000 == seen(pos + prevdir, prevdir * Complex(-1, 0)) then
+                    backtrack.enqueue((pos + prevdir, prevdir))
+
+        bestpath.size
 
     @main def run(): Unit =
         println(partOne())
+        println(partTwo())

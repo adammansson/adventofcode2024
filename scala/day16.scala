fe3d9d910a42bdade583bb46bc62134c1c6ffee1
Adam Månsson <adam.s.mansson@gmail.com>
2024-12-17 21:56:13 +0100

    Improve day16 in Scala

--- scala/day16.scala
+++ scala/day16.scala
@@ -17,38 +17,40 @@ object Day16:
 
         def find(e: T): Complex =
             Complex(data(data.indexWhere(_.contains(e))).indexOf(e),
-                data.indexWhere(_.contains(e)))
+                    data.indexWhere(_.contains(e)))
 
         override def toString(): String =
             data.map(_.mkString).mkString("\n")
 
     val path = os.pwd / ".." / "input" / "day16" / "input1.txt"
     val input: Vector[Vector[Char]] = os.read.lines(path).map(_.toVector).toVector
-    val dirs = Seq(Complex(1, 0), Complex(-1, 0), Complex(0, 1), Complex(0, -1))
     val map = Matrix(input)
     val start: Complex = map.find('S')
     val end: Complex = map.find('E')
 
-    val seen: Map[(Complex, Complex), Long] =
-        val currentlySeen = mutable.Map[(Complex, Complex), Long]()
+    val bests: Map[Complex, Map[Complex, Long]] =
+        val currentBests = mutable.Map[Complex, mutable.Map[Complex, Long]]()
         val queue = mutable.Queue[(Complex, Complex, Long)]((start, Complex(1, 0), 0))
         while queue.nonEmpty do
             val (pos, dir, points) = queue.dequeue
-            if currentlySeen.getOrElse((pos, dir), Long.MaxValue) > points then
-                currentlySeen((pos, dir)) = points
+
+            if !currentBests.contains(pos) then
+                currentBests(pos) = mutable.Map[Complex, Long]()
+
+            if currentBests(pos).getOrElse(dir, Long.MaxValue) > points then
+                currentBests(pos) += dir -> points
                 if map(pos + dir) != '#' then
                     queue.enqueue((pos + dir, dir, points + 1))
                 queue.enqueue((pos, dir * Complex(0, 1), points + 1000))
                 queue.enqueue((pos, dir * Complex(0, -1), points + 1000))
 
-        currentlySeen.toMap
-
-    def bestdir(pos: Complex): Complex =
-        val min = dirs.map(dir => seen(pos, dir)).min
-        dirs.find(dir => seen(pos, dir) == min).get
+        currentBests.map((k, v) => k -> v.toMap).toMap
 
     def partOne(): Long =
-        seen((end, bestdir(end)))
+        bests(end).minBy(_._2)._2
+
+    def bestdir(pos: Complex): Complex =
+        bests(pos).minBy(_._2)._1
 
     def partTwo(): Long =
         val bestpath = mutable.Set[Complex]()
@@ -60,9 +62,8 @@ object Day16:
                 val dir = bestdir(pos) * Complex(-1, 0)
                 backtrack.enqueue((pos + dir, dir))
 
-                if dir != prevdir &&
-                   map(pos + prevdir) != '#' &&
-                   seen(pos + dir, dir * Complex(-1, 0)) + 1000 == seen(pos + prevdir, prevdir * Complex(-1, 0)) then
+                if dir != prevdir && map(pos + prevdir) != '#' &&
+                   bests(pos + dir)(dir * Complex(-1, 0)) + 1000 == bests(pos + prevdir)(prevdir * Complex(-1, 0)) then
                     backtrack.enqueue((pos + prevdir, prevdir))
 
         bestpath.size
